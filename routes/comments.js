9daad507fb4ccddc27d87a49504177759634112a
edgardavidcure <102256702+edgardavidcure@users.noreply.github.com>
2023-09-29 15:50:40 -0700

    added validation and handled errors

--- routes/comments.js
+++ routes/comments.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
-
+const { commentValidation, validate } = require("../validation");
 const commentsController = require("../controllers/comments");
 
 router.get("/", commentsController.getAllComments);
 router.get("/:id", commentsController.getSingleComment);
-router.put("/:id", commentsController.updateSingleComment);
-router.post("/", commentsController.addComment);
+router.put(
+  "/:id",
+  commentValidation,
+  validate,
+  commentsController.updateSingleComment
+);
+router.post("/", commentValidation, validate, commentsController.addComment);
 router.delete("/:id", commentsController.deleteSingleComment);
 router.delete("/", commentsController.deleteAllComments);
 
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const { userValidation, validate } = require("../validation");
 
 const usersController = require("../controllers/users");
 
 router.get("/", usersController.getAllUsers);
 router.get("/:id", usersController.getSingleUser);
-router.put("/:id", usersController.updateSingleUser);
-router.post("/", usersController.addUser);
+router.put("/:id", userValidation, validate, usersController.updateSingleUser);
+router.post("/", userValidation, validate, usersController.addUser);
 router.delete("/:id", usersController.deleteSingleUser);
 router.delete("/", usersController.deleteAllUsers);
 
